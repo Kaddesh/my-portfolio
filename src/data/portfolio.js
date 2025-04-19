a1fbe93d217753200f999a701703f95b90a1780d
Oludamola <oludamolaijimade@gmail.com>
2025-04-20 00:29:57 +0100

    re-arraged project

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,6 +2,25 @@
 
 
 const PortfolioItems = [
+    
+    {
+      hrefLink:  'https://www.smartparrot.ai/',
+      hrefGit: '',
+      imgSrc: '/assets/parrot3.PNG',
+      mobileImgSrc: '/assets/smartparrot-mobile.PNG',
+      altText: 'parrotAi',
+      projectTitle: 'Smartparrot',
+      description: 'One app to manage all social content post, it helps you to generate ai prompt for all your social media content and helps you to schedule post, assign your post and also tract all your social media content on all social media platforms.',
+    },
+    {
+      hrefLink:'https://my-portfolio-red-zeta-15.vercel.app/',
+      hrefGit: 'https://github.com/Kaddesh/Recipe-Finder',
+      imgSrc: '/assets/recipe-share.PNG',
+      mobileImgSrc: '/assets/recipe-share.PNG',
+      altText: 'recipeApp',
+      projectTitle: 'RecipeFinder',
+      description: 'The app is a recipe finder that allows users to search for recipes by name or ingredient. It provides a list of recipes with images, names, and links to the recipe details. Users can also filter recipes by dietary restrictions and cuisine types.',
+    },
     {
       hrefLink:'https://kaddesh.github.io/EShopsy/',
       hrefGit: 'https://github.com/Kaddesh/EShopsy',
@@ -12,13 +31,13 @@ const PortfolioItems = [
       description: 'An e-commerce website that dynamically display products with amazing animation. The website allows users to use light or dark mode with ease and show modal for ordering of products..',
     },
     {
-      hrefLink:  'https://www.smartparrot.ai/',
-      hrefGit: '',
-      imgSrc: '/assets/parrot3.PNG',
-      mobileImgSrc: '/assets/smartparrot-mobile.PNG',
-      altText: 'parrotAi',
-      projectTitle: 'Smartparrot',
-      description: 'One app to manage all social content post, it helps you to generate ai prompt for all your social media content and helps you to schedule post, assign your post and also tract all your social media content on all social media platforms.',
+      hrefLink:  'https://kaddesh.github.io/easybank-/',
+      hrefGit: 'https://github.com/Kaddesh/easybank-',
+      imgSrc: '/assets/easybuy1.PNG',
+      mobileImgSrc: 'assets/easybank-mobile.PNG',
+      altText: 'EasyBuy app',
+      projectTitle: 'EasyBank',
+      description: 'The app is a landing page that provides users with an optimal layout depending on their device screen size. It displays hover states for all interactive elements on the page.',
     },
     {
       hrefLink:'https://taskye-kaddeshs-projects.vercel.app/',
@@ -38,24 +57,7 @@ const PortfolioItems = [
       projectTitle: 'Tracker',
       description: 'The app is an IP Address Tracker that provides users with the ability to view their own IP address and location on a map upon initial page load. Users can search for any IP address or domain to see key information and its location.',
     },
-    {
-      hrefLink:  'https://kaddesh.github.io/easybank-/',
-      hrefGit: 'https://github.com/Kaddesh/easybank-',
-      imgSrc: '/assets/easybuy1.PNG',
-      mobileImgSrc: 'assets/easybank-mobile.PNG',
-      altText: 'EasyBuy app',
-      projectTitle: 'EasyBank',
-      description: 'The app is a landing page that provides users with an optimal layout depending on their device screen size. It displays hover states for all interactive elements on the page.',
-    },
-    {
-      hrefLink:'https://my-portfolio-red-zeta-15.vercel.app/',
-      hrefGit: 'https://github.com/Kaddesh/Recipe-Finder',
-      imgSrc: '/assets/recipe-share.PNG',
-      mobileImgSrc: '/assets/recipe-share.PNG',
-      altText: 'recipeApp',
-      projectTitle: 'RecipeFinder',
-      description: 'The app is a recipe finder that allows users to search for recipes by name or ingredient. It provides a list of recipes with images, names, and links to the recipe details. Users can also filter recipes by dietary restrictions and cuisine types.',
-    },
+  
   ];
 
   export default PortfolioItems
